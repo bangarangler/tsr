087bed56d5ce5f97fb5ee248786506b5c3abd624
Jon <bangarangler@gmail.com>
2020-11-15 12:00:37 -0500

    refs with ts in react

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Input = () => {
   // const [name, setName] = useState<string | null>(null)
   // const [name, setName] = useState<string>("")
   const [name, setName] = useState("");
+  // !null Read Only
+  const ref = useRef<HTMLInputElement>(null!);
+  // not common
+  // const ref = useRef<HTMLInputElement | null>(null);
+  if (ref && ref.current) {
+    console.log("ref.current :>> ", ref.current.value);
+  }
   return (
-    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <input
+      type="text"
+      ref={ref}
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+    />
   );
 };
 
